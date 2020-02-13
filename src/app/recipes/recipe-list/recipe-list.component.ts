1979b60bc52409377e64b4dda1b28ce5f9c3e35d
Austin Cumberlander <austin.cumberlander@syndigo.com>
2020-02-12 21:43:43 -0600

    Added shopping-list and recipe services.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, EventEmitter, Output } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Recipe } from "../recipe.model";
+import { RecipeService } from "../recipe.service";
 
 @Component({
   selector: "app-recipe-list",
@@ -7,25 +8,11 @@ import { Recipe } from "../recipe.model";
   styleUrls: ["./recipe-list.component.scss"]
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  public recipes: Recipe[] = [
-    new Recipe(
-      "A Test Recipe",
-      "This is simply a test",
-      "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg"
-    ),
-    new Recipe(
-      "A Second Test Recipe",
-      "This is simply another test",
-      "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg"
-    )
-  ];
+  public recipes: Recipe[];
 
-  constructor() {}
+  constructor(private recipeService: RecipeService) {}
 
-  ngOnInit() {}
-
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
+  ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 }
